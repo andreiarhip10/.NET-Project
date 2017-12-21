efa979b811677395541a9518ece2d6a63ab083e3
vladutc <vladut.cojocariu@info.uaic.ro>
2017-12-21 06:17:03 +0200

    Added some contents for mockup

---
+++ DAVA-Angular/src/app/account.model.ts
@@ -0,0 +1,14 @@
+export class Account {
+
+  public id:number
+
+  public username:string
+
+  public email:string
+
+  public constructor(id:number, username:string, email:string){
+    this.id = id
+    this.username = username
+    this.email = email
+ }
+}
--- DAVA-Angular/src/app/app.component.ts
+++ DAVA-Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Account } from './account.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private _selected:Array<boolean> = []
+  private _accounts:Array<Account> = [
+    // {
+    //   id:1,
+    //   username:"user1",
+    //   email:"user1@gmail.com"
+    // },
+    // new Account(2,"user2","user2@gmail.com")
+  ]
+  private _nextId=1
+  private createAcc(usernameEl:string, emailEl:string)
+  {
+    this._accounts.push(new Account(this._nextId,usernameEl,emailEl))
+    this._selected.push(false) //default not selected
+    this._nextId++
+    usernameEl=""
+    emailEl=""
+  }
+  private removeAcc(index:number){
+    this._accounts.splice(index,1)
+    this._selected.splice(index,1)
+  }
+  private select(index:number)
+  {
+    this._selected[index]=!this._selected[index]
+  }
 }
