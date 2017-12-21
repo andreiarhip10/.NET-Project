483c7c6a3bf9f1a3b59e15461575094e541938bf
vladutc <vladut.cojocariu@info.uaic.ro>
2017-12-21 07:29:42 +0200

    Added bootstrap styles and components

--- DAVA-Angular/src/app/app.component.ts
+++ DAVA-Angular/src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent {
     // new Account(2,"user2","user2@gmail.com")
   ]
   private _nextId=1
+  private isEmpty=true
   private createAcc(usernameEl:string, emailEl:string)
   {
     this._accounts.push(new Account(this._nextId,usernameEl,emailEl))
@@ -32,5 +33,17 @@ export class AppComponent {
   private select(index:number)
   {
     this._selected[index]=!this._selected[index]
+  }
+  private isaccountsempty():boolean
+  {
+    if(this._accounts.length==0)
+    {
+      this.isEmpty=false
+    }
+    else
+    {
+      this.isEmpty=true
+    }
+    return this.isEmpty
   }
 }
